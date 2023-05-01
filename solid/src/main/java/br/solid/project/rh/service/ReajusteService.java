76a2e60d62530020bdc6ed25f55c4d09255c596b
Charles Araujo <callmatos@gmail.com>
2023-05-01 13:34:44 -0400

    Valida meses para reajuste 6 meses.

--- solid/src/main/java/br/solid/project/rh/service/ReajusteService.java
+++ solid/src/main/java/br/solid/project/rh/service/ReajusteService.java
@@ -2,6 +2,8 @@ package br.solid.project.rh.service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import br.solid.project.rh.exception.ValidacaoException;
 import br.solid.project.rh.model.Funcionario;
@@ -13,10 +15,17 @@ public class ReajusteService {
         
         BigDecimal salarioAtual = func.getSalario();
 
+        // Valida o percentual
         BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
-		
         if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
 			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+		}
+
+        // Valida se o novo reajuste foi antes de 6 meses.
+        LocalDate dataUltimoReajuste = func.getDataUltimoReajuste();
+        long mesesDeseUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, LocalDate.now());
+        if (mesesDeseUltimoReajuste < 6) {
+			throw new ValidacaoException("Intervalo entre reajuste abaixo de 6 meses.");
 		}
 
         BigDecimal novoSalarioAtualizado = salarioAtual.add(aumento);
